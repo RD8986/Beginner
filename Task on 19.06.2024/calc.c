645a3d1c944c2a907b380b5547531025e29b0f2a
= <=>
2024-06-19 17:38:01 +0530

    Basic Calculator

---
+++ Task on 19.06.2024/calc.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+int main()
+{
+    char op;
+    double first, second, result;
+    printf("Enter two Numbers : ");
+    scanf("%lf %lf", &first, &second);
+    while (1)
+    {
+        printf("\n Enter an operator (+, -, *, /) or 'x' to exit: ");
+        scanf(" %c", &op);
+
+        if (op == 'x')
+        {
+            break;
+        }
+
+        if (op == '+')
+        {
+            result = first + second;
+            printf("%.1lf + %.1lf = %.1lf", first, second, result);
+        }
+        else if (op == '-')
+        {
+            result = first - second;
+            printf("%.1lf - %.1lf = %.1lf", first, second, result);
+        }
+        else if (op == '*')
+        {
+            result = first * second;
+            printf("%.1lf * %.1lf = %.1lf", first, second, result);
+        }
+        else if (op == '/')
+        {
+            if (second != 0.0)
+            {
+                result = first / second;
+                printf("%.1lf / %.1lf = %.1lf", first, second, result);
+            }
+            else
+            {
+                printf("Error Division by zero");
+            }
+        }
+        else
+        {
+            printf("Error Invalid operator");
+        }
+    }
+
+    printf("Calculator program terminated.\n");
+    return 0;
+}
